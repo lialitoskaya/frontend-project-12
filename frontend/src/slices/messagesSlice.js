85192d6e507c5a97d2a138a3da9b2faab3f4c1ce
lialitoskaya <liyarheon@gmail.com>
2024-02-08 02:09:03 +0500

    added axios functions

--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -1,24 +1,24 @@
-import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const messagesAdapter = createEntityAdapter();
 
 export const axiosMessages = async (token) => {
-  const messagesRes = await axios.get("/api/v1/messages", {
+  const messagesRes = await axios.get('/api/v1/messages', {
     headers: { Authorization: `Bearer ${token}` },
   });
   return messagesRes.data;
 };
 
 const messagesSlice = createSlice({
-  name: "messages",
+  name: 'messages',
   initialState: messagesAdapter.getInitialState(),
   reducers: {
     addMessages: messagesAdapter.addMany,
     addMessage: messagesAdapter.addOne,
-    updateMessages: messagesAdapter.setAll
+    updateMessages: messagesAdapter.setAll,
   },
-})
+});
 
 export default messagesSlice.reducer;
 export const { addMessages, addMessage, updateMessages } = messagesSlice.actions;
