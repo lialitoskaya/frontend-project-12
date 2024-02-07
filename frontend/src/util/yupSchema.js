b6cb3694fcfba264958aa9591626fdba87ee9d31
lialitoskaya <liyarheon@gmail.com>
2024-02-08 02:01:05 +0500

    added yup authorization validation schema

--- frontend/src/util/yupSchema.js
+++ frontend/src/util/yupSchema.js
@@ -1,11 +1,29 @@
-import * as yup from "yup";
+import * as yup from 'yup';
+import i18n from './i18n';
+
 const schema = (channelsNames) => yup.object().shape({
-    name: yup
-      .string()
-      .required("Поле должно быть заполнено")
-      .min(3, "Название должно иметь от 3 до 20 символов")
-      .max(22, "Название должно иметь от 3 до 22 символов")
-      .notOneOf(channelsNames, "Канал с таким названием уже существует"),
+  name: yup
+    .string()
+    .required(i18n.t('chatPage.channels.errors.required'))
+    .min(3, i18n.t('chatPage.channels.errors.nameLength'))
+    .max(22, i18n.t('chatPage.channels.errors.nameLength'))
+    .notOneOf(channelsNames, i18n.t('chatPage.channels.errors.notUniqName')),
+});
+
+export const authorizationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required(i18n.t('signUpForm.errors.required'))
+    .min(3, i18n.t('signUpForm.errors.usernameLength'))
+    .max(20, i18n.t('signUpForm.errors.usernameLength')),
+  password: yup
+    .string()
+    .required('Обязательное поле')
+    .min(6, i18n.t('signUpForm.errors.passwordLength')),
+  confirmPassword: yup
+    .string()
+    .required('Обязательное поле')
+    .oneOf([yup.ref('password')], i18n.t('signUpForm.errors.confirmPassword')),
 });
 
 export default schema;
