4509dcb81332dd543281145c62ca3ce0b1cf0b53
lialitoskaya <liyarheon@gmail.com>
2024-02-10 00:29:39 +0500

    created init i18n func for initialization app

--- frontend/src/util/i18n.js
+++ frontend/src/util/i18n.js
@@ -4,13 +4,15 @@ import { ru } from './resources';
 
 const i18n = i18next.createInstance();
 
-i18n.use(initReactI18next).init({
-  resources: { ru },
-  fallbackLng: 'ru',
-  lng: 'ru',
-  interpolation: {
-    escapeValue: false,
-  },
-});
+export const init = async () => {
+  await i18n.use(initReactI18next).init({
+    resources: { ru },
+    fallbackLng: 'ru',
+    lng: 'ru',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+};
 
 export default i18n;
