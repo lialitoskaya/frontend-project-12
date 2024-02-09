633215c1e0a3cb6dda383305867e17459a9562c1
lialitoskaya <liyarheon@gmail.com>
2024-02-10 00:23:02 +0500

    added extraReducer deletion of messages when deleting a channel

--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -1,24 +1,35 @@
-import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const messagesAdapter = createEntityAdapter();
 
 export const axiosMessages = async (token) => {
-  const messagesRes = await axios.get('/api/v1/messages', {
+  const messagesRes = await axios.get("/api/v1/messages", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return messagesRes.data;
 };
 
 const messagesSlice = createSlice({
-  name: 'messages',
+  name: "messages",
   initialState: messagesAdapter.getInitialState(),
   reducers: {
     addMessages: messagesAdapter.addMany,
     addMessage: messagesAdapter.addOne,
     updateMessages: messagesAdapter.setAll,
   },
+  extraReducers: (build) => {
+    build.addCase("channels/removeChannel", (state, { payload }) => {
+      const deletedChannelId = payload;
+      Object.values(state.entities).forEach((m) => {
+        if (m.channelId === deletedChannelId) {
+          messagesAdapter.removeOne(state, m.id);
+        }
+      });
+    });
+  },
 });
 
 export default messagesSlice.reducer;
-export const { addMessages, addMessage, updateMessages } = messagesSlice.actions;
+export const { addMessages, addMessage, updateMessages } =
+  messagesSlice.actions;
