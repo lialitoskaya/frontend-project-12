313b2b559cd8554a31b8b6ce297681923fc9c047
liya <liyarheon@gmail.com>
2024-02-28 20:36:19 +0300

    rename channels yup schema && fix auth schema bug

--- frontend/src/util/yupSchema.js
+++ frontend/src/util/yupSchema.js
@@ -1,29 +1,30 @@
-import * as yup from 'yup';
-import i18n from './i18n';
+import * as yup from "yup";
+import i18n from "./i18n.js";
 
-const schema = (channelsNames) => yup.object().shape({
-  name: yup
-    .string()
-    .required(i18n.t('chatPage.channels.errors.required'))
-    .min(3, i18n.t('chatPage.channels.errors.nameLength'))
-    .max(22, i18n.t('chatPage.channels.errors.nameLength'))
-    .notOneOf(channelsNames, i18n.t('chatPage.channels.errors.notUniqName')),
-});
+const channelNameSchema = (channelsNames) =>
+  yup.object().shape({
+    name: yup
+      .string()
+      .required(i18n.t("chatPage.channels.errors.required"))
+      .min(3, i18n.t("chatPage.channels.errors.nameLength"))
+      .max(22, i18n.t("chatPage.channels.errors.nameLength"))
+      .notOneOf(channelsNames, i18n.t("chatPage.channels.errors.notUniqName")),
+  });
 
-export const authorizationSchema = yup.object().shape({
+export const authorizationSchema = () => yup.object().shape({
   username: yup
     .string()
-    .required(i18n.t('signUpForm.errors.required'))
-    .min(3, i18n.t('signUpForm.errors.usernameLength'))
-    .max(20, i18n.t('signUpForm.errors.usernameLength')),
+    .required(i18n.t("signUpForm.errors.required"))
+    .min(3, i18n.t("signUpForm.errors.usernameLength"))
+    .max(20, i18n.t("signUpForm.errors.usernameLength")),
   password: yup
     .string()
-    .required('Обязательное поле')
-    .min(6, i18n.t('signUpForm.errors.passwordLength')),
+    .required(i18n.t("signUpForm.errors.required"))
+    .min(6, i18n.t("signUpForm.errors.passwordLength")),
   confirmPassword: yup
     .string()
-    .required('Обязательное поле')
-    .oneOf([yup.ref('password')], i18n.t('signUpForm.errors.confirmPassword')),
+    .required(i18n.t("signUpForm.errors.required"))
+    .oneOf([yup.ref("password")], i18n.t("signUpForm.errors.confirmPassword")),
 });
 
-export default schema;
+export default channelNameSchema;
